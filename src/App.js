ae0c333775ec309757ca2eca582b0db92069d387
pitunti <pi121992@gmail.com>
2021-08-31 04:48:32 -0500

    Subida  a git solo con side bar y el header con fondo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
-import './App.css';
+
+import './App.scss';
+import { SideBar } from './components/sideBar/SideBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+     <div className="pagina">
+        <header>
+           <div className="title">
+              <h1>Chopsticks</h1>
+              <small>Sushi y mas!</small>
+           </div>
+           <SideBar/>
+        </header>
+
+        <main>
+          Hola
+        </main>
+     </div>
+   </>
   );
 }
 
---
+++ src/components/sideBar/SideBar.js
@@ -0,0 +1,47 @@
+import React from 'react'
+
+import "./SideBar.scss"
+
+export const SideBar = ({ className }) => {
+    const openMenu = () => {
+        const menu = document.querySelector(".Nav")
+        const main = document.querySelector("main")
+        menu.classList.toggle("expanded");
+        main.classList.toggle("collapse");
+        console.log(menu)
+    }
+    return (
+        <nav className="Nav">
+            {/* <div className="logo">
+                 <a href="#" className="">
+                 <img src="https://cdn.iconscout.com/icon/free/png-512/plex-226049.png" alt="" className="logo-img"/>
+                 </a>
+             </div> */}
+
+            <i className="fas fa-bars btn-menu" onClick={openMenu}></i>
+           <div className="header">
+                
+           <img src="https://scontent-dfw5-1.xx.fbcdn.net/v/t39.30808-6/239387960_225203896278087_4109352718933834206_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=_6mD7l8rGqMAX8xUQIB&_nc_ht=scontent-dfw5-1.xx&oh=6729bd9e30deef9e777133b62e46b3e8&oe=613350C6" alt="" className="" />
+                    <span className="userName">
+                       Chopsticks
+                    </span>
+           </div>
+
+            <ul className={className}>
+              
+                <li>
+                    <i class="fas fa-home"></i> <span>Home</span>
+                </li>
+
+
+
+            </ul>
+
+            <div className="perfil-img">
+
+            </div>
+        </nav>
+
+
+    )
+}
